0a14949e7c659b58305dc0a40ab9a01c21ed3842
VORTEX <VORTEX@VORTEX-PC>
2017-09-16 17:04:25 +0300

    This is my first program in Java

---
+++ Java6PahontuLoredana/src/firstprogram/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * My first program written in Java
+ */
+package firstprogram;
+
+/**
+ * This is my first class
+ * @author VORTEX
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // say Helloww World
+        System.out.println("Hello World");
+    }
+    
+}
